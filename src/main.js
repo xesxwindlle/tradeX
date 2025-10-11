d999eac2d3b91fbd0af95ccc931f4deacb70ce3b
Eve <xesxwindlle@gmail.com>
2025-10-11 19:33:56 +0800

    <create> src/stores

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createPinia } from 'pinia';
 import axios from 'axios';
 import "@/assets/main.css";
 
-createApp(App).mount('#app');
+
+const pinia = createPinia();
+createApp(App).use(pinia).mount('#app');
 
 
---
+++ src/stores/watchlist_store.js
@@ -0,0 +1,5 @@
+import { defineStore } from "pinia"
+const watchlist_store = defineStore("watchlist", () => {
+
+}
+);
