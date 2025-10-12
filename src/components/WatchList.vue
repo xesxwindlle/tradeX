d47d10880b911abdc735d2d7e54f6c028d8d5e07
Eve <xesxwindlle@gmail.com>
2025-10-12 16:47:14 +0800

    <implement> WatchList.vue

--- src/components/WatchList.vue
+++ src/components/WatchList.vue
@@ -5,17 +5,24 @@ import { watchlistStore } from '@/stores/watchlist_store';
 const wls = watchlistStore();
 
 onMounted (() => {
-     wls.displayWatchings();
+     wls.refreshWatchings();
 })
-
 </script>
 
 <template>
      <div class="panel">
         <h2 class="panel-title">Watchlist</h2>
-        <li v-for="watching in wls.watchings" >
-        <div class="list-item"><span>{{ watching }}</span> <span>{{watching }}</span></div>
-
+        <li v-for="symbol in wls.watchings.keys()">
+            <div class="list-item">
+                <li>{{ symbol }}</li>
+                <li>Current Price : {{ wls.getCurrenPrice(symbol) }}</li>
+                <li>Pirce Change : {{ wls.getDayChange(symbol) }}</li>
+                <li>Percent Chnage : {{ wls.getPercentChange(symbol) }}</li>
+                <li>Day High : {{ wls.getDayHigh(symbol) }}</li>
+                <li>Day Low : {{ wls.getDayLow(symbol) }}</li>
+                <li>Opening Price : {{ wls.getDayOpen(symbol) }}</li>
+                <li>Previous Day Closign Price : {{ wls.getPrevClose(symbol) }}</li>
+            </div>
         </li>
     </div>
 </template>
--- src/stores/watchlist_store.js
+++ src/stores/watchlist_store.js
@@ -2,7 +2,7 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 import { getStockQuote } from "@/library/util";
 
-const watchings = ref(new Set());
+const watchings = ref(new Map());
 // const stock = ref("");
 
 const watchlistStore = defineStore("watchlist", () => {
@@ -10,24 +10,62 @@ const watchlistStore = defineStore("watchlist", () => {
 
   function addWatching(symbol) { 
     //if the stock exists...
-    watchings.value.add(symbol);
-    console.log(watchings.value);
+    watchings.value.set(symbol, {});
+    fetchQuote(symbol);
+    // console.log(watchings.value.get(symbol));
   }
 
-  function displayWatchings() {
-    // console.log(watchings);
+  async function fetchQuote(symbol) {
+    const url = getStockQuote(symbol);
+    const res = await fetch(url);
+    const quote = await res.json();
+    watchings.value.set(symbol, quote);
+  }
+
+  function refreshWatchings() {
     setInterval(async () => {
-      for (let symbol of watchings.value) {
-      const url = getStockQuote(symbol);
-      const res = await fetch(url);
-      const data = await res.json();
-      console.log(data);
-    }
-  }, 5000);
-}
+      for (let symbol of watchings.value.keys()) {
+        fetchQuote(symbol);
+        console.log(getQuote(symbol));
+      }
+    }, 5000); 
+  }
+  
+  
+  function getQuote(symbol) {
+    return watchings.value.get(symbol);
+   }
+   
+  function getCurrenPrice(symbol) {
+    return getQuote(symbol).c;
+  }
 
+  function getDayChange(symbol) {
+    return getQuote(symbol).d;
+  }
 
-  return { addWatching, watchings, displayWatchings};
+  function getPercentChange(symbol) {
+    return getQuote(symbol).dp;
+  }
+
+  function getDayHigh(symbol) {
+    return getQuote(symbol).h;
+  }
+
+  function getDayLow(symbol) {
+    return getQuote(symbol).l;
+  }
+
+  function getDayOpen(symbol) {
+    return getQuote(symbol).o;
+  }
+
+  function getPrevClose(symbol) {
+    return getQuote(symbol).pc;
+  }
+ 
+  return { addWatching, watchings, fetchQuote, refreshWatchings, getQuote, getCurrenPrice,
+     getDayChange, getPercentChange, getDayHigh, getDayLow, getDayOpen, getPrevClose};
 });
 
 export { watchlistStore };
